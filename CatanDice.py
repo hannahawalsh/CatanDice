e5e9b1f3d24c25aa27c573a9fd2f10bcaa185bdf
Hannah Walsh <hannahawalsh@github.com>
2020-10-30 23:46:46 -0500

    cleaned up by using new beta columns and containers

--- CatanDice.py
+++ CatanDice.py
@@ -6,77 +6,57 @@ import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
 
-st.beta_set_page_config(page_title="Gambler's Dice")
-
 
 def main():
-    # Page Title
+    """ """
+    ### Set up sidebar
+    st.sidebar.title("Game Options")
+    apply_placeholder = st.sidebar.empty()
+    players_radio = st.sidebar.radio("Number of Players", [3, 4], index=1)
+    player1 = st.sidebar.text_input("Player 1", "Player 1")
+    player2 = st.sidebar.text_input("Player 2", "Player 2")
+    player3 = st.sidebar.text_input("Player 3", "Player 3")
+    players = {0: player1, 1: player2, 2: player3}
+    if players_radio == 4:
+        player4 = st.sidebar.text_input("Player 4", "Player 4")
+        players[3] = player4
+    random_rate_slider = st.sidebar.slider("Randomness Parameter", 0., 1., 0.15)
+    convergence_rate_slider = st.sidebar.slider("Convergence Rate", 0., 1., 0.5)
+    convergence_rate_slider = convergence_rate_slider * 250 + 200
+    random_turns_slider = st.sidebar.slider("Starting Random Turns", 1, 32, 8)
+
+
+    ### Set up main page
     title_style = ("style='text-align: center; font-size: 4.0em; "
                    "color: gold; background-color: maroon; "
                    "font-family: Georgia;'")
-    st.markdown(f"<h1 {title_style}> CATAN DICE </h1>",
-                unsafe_allow_html=True)
+    st.markdown(f"<h1 {title_style}> CATAN DICE </h1>", unsafe_allow_html=True)
 
-    # Get cached variables
+    number_text = st.empty()
+    player_name_text = st.empty()
+    display_space = st.beta_container()
+
+    _, roll_space, _ = st.beta_columns(3)
+    roll_button = roll_space.button("Roll!")
+    b1, b2, b3 = st.beta_columns(3)
+    reset_button = b1.button("Reset")
+    undo_button = b2.button("Undo")
+    stats_button = b3.button("Statistics")
+    ###
+    trial_button = b2.button("**Roll 500 times**")
+    ###
+
+
+    ### Get cached variables
     roll_history = get_roll_history()
     player_history = get_player_history()
 
-    # Set up sidebar layout
-    st.sidebar.title("Game Options")
-    apply_placeholder = st.sidebar.empty()
-    radio = st.sidebar.empty()
-    p1 = st.sidebar.empty()
-    p2 = st.sidebar.empty()
-    p3 = st.sidebar.empty()
-    p4 = st.sidebar.empty()
-    rand_rate = st.sidebar.empty()
-    conv_rate = st.sidebar.empty()
-    rand_turns = st.sidebar.empty()
-
-    # Set up main page layout and buttons
-    number_text = st.empty()
-    player_name_text = st.empty()
-    spot_1 = st.empty()
-    spot_2 = st.empty()
-    spot_3 = st.empty()
-    roll_button = st.button("Roll!")
-    undo_button = st.button("Undo Roll")
-    reset_button = st.button("Reset Game")
-    stats_button = st.button("Game Statistics")
-    ###
-    # trial_button = st.button("Roll 100 times")
-    ###
-
-    # Add in widgets
-    players_radio = radio.radio("Number of Players", [3, 4],
-                                        index=1, format_func=int)
-    player1 = p1.text_input("Player 1", "Player 1")
-    player2 = p2.text_input("Player 2", "Player 2")
-    player3 = p3.text_input("Player 3", "Player 3")
-    players = {0: player1, 1: player2, 2: player3}
-    if players_radio == 4:
-        player4 = p4.text_input("Player 4", "Player 4")
-        players[3] = player4
-    random_rate_slider = rand_rate.slider("Randomness Parameter",
-                                            0.0, 1.0, 0.15)
-    convergence_rate_slider = conv_rate.slider("Convergence Rate",
-                                                0.0, 1.0, 0.5)
-    convergence_rate_slider = convergence_rate_slider * 250 + 200
-    random_turns_slider = rand_turns.slider("Starting Random Turns", 1, 32, 8)
-
-
-    # define styles
-    name_style = ("style='text-align: center; font-size: 4.0em; "
-                  "font-family: Arial; padding: 0px'")
-    num_style = lambda n: ("style='text-align: center; font-size: 12.0em; "
-                           "font-family: Arial Black; padding: 0px; "
-                           f"color: {'red' if n in [6, 8] else 'black'};'")
-
-    # Do action based on button pressed
+    # get_number_text, get_name_text
+    ### Define actions
     if not roll_history:
         number_text.image(dice_image, use_column_width=True)
-        player_name_text.markdown(f"<h1 {name_style}> Roll to start game! "
-                                  "</h1>", unsafe_allow_html=True)
+        player_name_text.markdown(get_name_text("Roll to start game!"),
+                                  unsafe_allow_html=True)
     edit_params_button = apply_placeholder.button("Apply Changes")
 
     if roll_button:
@@ -94,81 +74,100 @@ def main():
         player_history.append(current_player)
 
         # Display the results
-        number_text.markdown(f"<h1 {num_style(next_roll)}> {next_roll} "
-                             "</h1>", unsafe_allow_html=True)
-        player_name_text.markdown(f"<h1 {name_style}> {player_name} </h1>",
+        number_text.markdown(get_number_text(next_roll), unsafe_allow_html=True)
+        player_name_text.markdown(get_name_text(player_name),
                                   unsafe_allow_html=True)
 
     elif undo_button:
-        roll_history.pop()
-        player_history.pop()
+        _ = roll_history.pop()
+        _ = player_history.pop()
 
         # Display the results
         if roll_history:
             prev_roll = roll_history[-1]
             prev_player = players[player_history[-1]]
-            number_text.markdown(f"<h1 {num_style(prev_roll)}> {prev_roll} "
-                                 "</h1>", unsafe_allow_html=True)
-            player_name_text.markdown(f"<h1 {name_style}> {prev_player} </h1>",
+            number_text.markdown(get_number_text(prev_roll),
+                                 unsafe_allow_html=True)
+            player_name_text.markdown(get_name_text(prev_player),
                                       unsafe_allow_html=True)
         else:
             number_text.image(dice_image, use_column_width=True)
-            player_name_text.markdown(f"<h1 {name_style}> Roll to start game! "
-                                      "</h1>", unsafe_allow_html=True)
+            player_name_text.markdown(get_name_text("Roll to start game!"),
+                                      unsafe_allow_html=True)
 
     elif reset_button:
         st.caching.clear_cache()
         number_text.image(dice_image, use_column_width=True)
-        player_name_text.markdown(f"<h1 {name_style}> Roll to start game! "
-                                  "</h1>", unsafe_allow_html=True)
-        apply_placeholder = apply_placeholder.empty()
+        player_name_text.markdown(get_name_text("Roll to start game!"),
+                                  unsafe_allow_html=True)
+        apply_placeholder.empty()
 
     elif edit_params_button:
         if player_history:
             current_player = players[player_history[-1]]
             current_roll = roll_history[-1]
-            number_text.markdown(f"<h1 {num_style(current_roll)}> {current_roll} "
-                             "</h1>", unsafe_allow_html=True)
-            player_name_text.markdown(f"<h1 {name_style}> {current_player} </h1>",
+            number_text.markdown(get_number_text(current_roll),
+                                 unsafe_allow_html=True)
+            player_name_text.markdown(get_name_text(current_player),
                                       unsafe_allow_html=True)
         else:
             number_text.image(dice_image, use_column_width=True)
-            player_name_text.markdown(f"<h1 {name_style}> Roll to start game! "
-                                      "</h1>", unsafe_allow_html=True)
+            player_name_text.markdown(get_name_text("Roll to start game!"),
+                                      unsafe_allow_html=True)
 
     elif stats_button:
-        player_name_text.markdown(f"<h1 {name_style}> Good Game! "
-                                  "</h1>", unsafe_allow_html=True)
+        player_name_text.markdown(get_name_text("Good Game!"),
+                                  unsafe_allow_html=True)
         player_names = [players[k] for k in sorted(players)]
-        result_fig, roll_stats, turn_count = analyze_results(roll_history,
-                                                              player_names)
-        spot_1.pyplot(fig=result_fig)
-        spot_2.write(roll_stats)
-        spot_3.write(turn_count)
+        rsts_fig, stats, turns = analyze_results(roll_history, player_names)
+        display_space.pyplot(fig=rsts_fig)
+        display_space.write(stats)
+        display_space.write(turns)
 
-    # elif trial_button:
-    #     for _ in range(100):
-    #         next_roll = dice_roll(roll_history, random_turns_slider,
-    #                               random_rate_slider, convergence_rate_slider)
-    #         roll_history.append(next_roll)
-    #     number_text.markdown(f"<h1 {num_style(next_roll)}> "
-    #                          f"{len(roll_history)} Rolls in Total"
-    #                          f"</h1>", unsafe_allow_html=True)
+    elif trial_button:
+        for _ in range(500):
+            next_roll = dice_roll(roll_history, random_turns_slider,
+                                  random_rate_slider, convergence_rate_slider)
+            roll_history.append(next_roll)
+        number_text.markdown(get_number_text(len(roll_history)),
+                             unsafe_allow_html=True)
+
+
 
 
 @st.cache(allow_output_mutation=True)
 def get_roll_history():
     return []
 
+
 @st.cache(allow_output_mutation=True)
 def get_player_history():
     return []
 
+
+def get_number_text(n):
+    """ Given a number, return the html for displaying it"""
+    color = "red" if n in [6, 8] else "black"
+    style = ("<h1 style='text-align: center; font-size: 12.0em; "
+             f"font-family: Arial Black; padding: 0px; color: {color};'> "
+             f"{n} </h1>")
+    return style
+
+
+def get_name_text(name):
+    """ Given a player's name, return the html for displaying it """
+    style = ("<h1 style='text-align: center; font-size: 4.0em; font-family:"
+             f" Arial; padding: 0px'> {name} </h1>")
+    return style
+
+
 def normalize_dict(D):
+    """ Return a dictionary with values normalized and missing values at 0 """
     new = {k: v / sum(D.values()) for k, v in D.items()}
     missing_vals = [x for x in range(2, 13) if x not in new.keys()]
     new = {**new, **{m: 0.0 for m in missing_vals}}
     return {k: new[k] for k in range(2, 13)}
+
 
 def dice_roll(roll_history, first, random_rate, convergence_rate):
     """
@@ -227,8 +226,13 @@ def analyze_results(roll_history, player_names):
 
     return freq_plot, roll_breakdown, turn_count
 
+
+
+
 if __name__ == "__main__":
     fair_weights = {**{i: (i-1)/36 for i in range(2, 8)}, **{i: (13-i)/36 for
                     i in range(8, 13)}}
     dice_image = Image.open("DicePic.png")
+    st.set_page_config(page_title="Gambler's Fallacy Dice",
+                       page_icon="🎲", layout="centered")
     main()
